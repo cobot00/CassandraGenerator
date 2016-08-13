8c446cf1a16ddfb57bd26361fc0c1f9866feea13
Ryoji Kobori <kobori75@gmail.com>
2016-08-13 16:24:26 +0900

    init

---
+++ src/main/java/com/cobot00/cassandra_generator/CassandraGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.cobot00.cassandra_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraGeneratorApplication.class, args);
+	}
+}
